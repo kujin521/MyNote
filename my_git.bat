458391f11b66570e96b83e287f7ccb259e10ecd1
库金 <1604323361@qq.com>
2021-09-09 09:18:11 +0800

    auto

--- my_git.bat
+++ my_git.bat
@@ -1,21 +1,4 @@
-@echo off
-
-echo "-------Begin-------"
-
-git status
-
-set /p msg=请输入提交注释:
-
 git add .
-
-git commit -m %msg%
-
+git commit -m "auto"
 git pull
-
 git push
-
-echo 推送成功：【%msg%】
-
-echo "--------End!--------"
-
-pause
